41537113105d779a9249fcb3a50d368645b3c4dd
Vince Masten <vmasten@me.com>
2021-03-31 23:25:54 -0700

    backend works

--- server.js
+++ server.js
@@ -10,8 +10,14 @@ const User = require('./models/Schema')
 mongoose.connect(process.env.MONGO_SERVER, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const app = express();
+app.use(express.json());
 app.use(cors());
+
 const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('db on and connected');
+});
 
 const vince = new User({
   email: 'vinnie1023@gmail.com',
@@ -23,12 +29,25 @@ vince.save();
 
 app.get('/', (req, res) => res.send("heyo"));
 app.get('/books', getBooks);
+app.post('/books', createBook)
 
 async function getBooks(request, response) {
   const email = request.query.email;
   await User.find({email: email}, function (err, items) {
     if (err) return console.error(err);
     response.status(200).send(items[0].books);
+  })
+}
+
+async function createBook(request, response) {
+  const { email, name, description, status } = request.body;
+  const newBook = {name: name, description: description, status: status}
+  
+  User.findOne({ email }, (err, item) => {
+    if(err) return console.error(err);
+    item.books.push(newBook);
+    item.save();
+    response.status(200).send(item.books);
   })
 }
 
